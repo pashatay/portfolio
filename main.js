fdb867b006f85b5dd5a6762a45830ca906dd1a10
Pasha Tay <pashabonilla@gmail.com>
2019-09-11 00:10:16 -0400

    changes

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
+
 const navSlide = () =>{
     const burger = document.querySelector('.hamburger');
     const nav = document.querySelector('.navbar-links');
-
+   console.log(nav);
     burger.addEventListener('click', () =>{
         console.log(nav);
         nav.classList.toggle('navbar-active');
     });
 }
 
+const clickMenu = () => {
+    const links = document.querySelectorAll('.navbar-links li')
+    console.log(links[0]);
+    links.forEach(function(el,idx){
+         el.style.animation = `navbar-links-fade 0.5s ease forwards ${idx/7 + 1}s`;
+    //   el.addEventListener('click', () => {
+    //      console.log(el);
+    //      el.classList.remove('navbar-active');
+    //   })
+    });
+}
+
 navSlide();
+clickMenu();
