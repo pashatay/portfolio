2282f56e2062a67f7d7c354cbcb7114eb5773a91
Pasha Tay <pashabonilla@gmail.com>
2019-09-12 20:08:21 -0400

    changes

--- main.js
+++ main.js
@@ -2,24 +2,31 @@
 const navSlide = () =>{
     const burger = document.querySelector('.hamburger');
     const nav = document.querySelector('.navbar-links');
-   console.log(nav);
+    const links = document.querySelectorAll('.navbar-links li')
+
+
     burger.addEventListener('click', () =>{
-        console.log(nav);
         nav.classList.toggle('navbar-active');
+
+        links.forEach(function(el,idx){
+            if(el.style.animation){
+               el.style.animation = '';
+            }else{
+              el.style.animation = `navbar-links-fade 0.5s ease forwards ${idx/ 7 + 1.5}s`;
+            }
     });
+});
 }
 
-const clickMenu = () => {
-    const links = document.querySelectorAll('.navbar-links li')
-    console.log(links[0]);
-    links.forEach(function(el,idx){
-         el.style.animation = `navbar-links-fade 0.5s ease forwards ${idx/7 + 1}s`;
-    //   el.addEventListener('click', () => {
-    //      console.log(el);
-    //      el.classList.remove('navbar-active');
-    //   })
-    });
-}
+// const clickMenu = () => {
+//     const links = document.querySelectorAll('.navbar-links li')
+
+//     //links.forEach(function(el,){
+//     //   el.addEventListener('click', () => {
+//     //      console.log(el);
+//     //      el.classList.remove('navbar-active');
+//     //   })
+//     //});
+// }
 
 navSlide();
-clickMenu();
