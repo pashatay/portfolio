7087b64065f67da541ee1119e6c8c01c77fd5b16
Pasha Tay <pashabonilla@gmail.com>
2019-09-15 22:55:15 -0400

    organize files

--- main.js
+++ main.js
@@ -1,36 +1,26 @@
-const burger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.navbar-links');
-    const links = document.querySelectorAll('.navbar-links li')
+const burger = $('.hamburger');
+const nav = $('.navbar-links');
+const links = $('.navbar-links li')
 
 const navSlide = () =>{
-    burger.addEventListener('click', () =>{
-      if(nav.className === 'navbar-active'){
-        nav.className = 'navbar-inactive'
-     }
-        nav.classList.toggle('navbar-active');
-
-        links.forEach((el,idx)=>{
-            if(el.style.animation){
-               el.style.animation = '';
-            }else{
-              el.style.animation = `navbarLinksFade 0.5s ease forwards ${idx/ 7 + 1.5}s`;
-              //console.log(idx/7+0.2);
-            }
-    });
-    //burger animation
-    burger.classList.toggle('toggle');
+    burger.on('click', () =>{
+      nav.toggleClass('navbar-active');
+      burger.toggleClass('toggle');
 });
 }
 
 const clickMenu = () => {
-    links.forEach(function(el,){
-      el.addEventListener('click', () => {
-         console.log(el);
-         nav.classList.remove('navbar-active');
-         burger.classList.remove('toggle');
-      })
-    });
+    links.on('click', () => {
+         console.log(links);
+         nav.removeClass('navbar-active');
+         burger.removeClass('toggle');
+      });
 }
 
-navSlide();
-clickMenu();
+const ready = () => {
+  navSlide();
+  clickMenu();
+}
+
+ready();
+
