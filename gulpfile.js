99fde80a3fe2f9cf5d9b9e9da0f8142edcab7ad4
Pasha Tay <pashabonilla@gmail.com>
2019-09-16 22:50:27 -0400

    changes

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp');
+const concatCss = require('gulp-concat-css');
+gulp.task('css', function() {
+  return gulp.src('css/*.css')
+    .pipe(concatCss('style.css'))
+    .pipe(gulp.dest('.'));
+});
