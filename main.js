b236afa26152a44a9b53db0e9e08aee0a032c38f
Pasha Tay <pashabonilla@gmail.com>
2019-10-08 19:29:27 -0400

    minor design changes

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-const burger = $('.hamburger');
-const nav = $('.navbar-links');
-const links = $('.navbar-links li')
+const burger = $(".hamburger");
+const nav = $(".navbar-links");
+const links = $(".navbar-links li");
 
-const navSlide = () =>{
-    burger.on('click', () =>{
-      nav.toggleClass('navbar-active');
-      burger.toggleClass('toggle');
-});
-}
+const navSlide = () => {
+  burger.on("click", () => {
+    nav.toggleClass("navbar-active");
+    burger.toggleClass("toggle");
+  });
+};
 
 const clickMenu = () => {
-    links.on('click', () => {
-         console.log(links);
-         nav.removeClass('navbar-active');
-         burger.removeClass('toggle');
-      });
-}
+  links.on("click", () => {
+    console.log(links);
+    nav.removeClass("navbar-active");
+    burger.removeClass("toggle");
+  });
+};
 
 const ready = () => {
   navSlide();
   clickMenu();
-}
+};
 
 ready();
-
